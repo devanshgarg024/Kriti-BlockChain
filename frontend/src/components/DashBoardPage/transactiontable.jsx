86148b165685f51d78965c0ba9149046d2367172
Devansh Garg <devanshgarg024@gmail.com>
2025-02-03 22:41:26 +0530

    machuda

--- frontend/src/components/DashBoardPage/transactiontable.jsx
+++ frontend/src/components/DashBoardPage/transactiontable.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./Transactionstable.css";
+import "./transactionstable.css";
 import axios from "axios";
 
 const TransactionsTable = (props) => {
